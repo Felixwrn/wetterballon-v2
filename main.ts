3d8ff81c326c67e242ea9afb393110742f3342ce
Felixwrn <104013013+Felixwrn@users.noreply.github.com>
2026-06-19 21:33:06 +0200

    Aktualisiere pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,11 +1,41 @@
-let licht = 0
-let temperatur = 0
-let zeit = 0
-serial.redirectToUSB()
+// SICHERHEITS-FUNKTION: Manueller Speicher-Reset vor dem Start
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Skull)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    datalogger.deleteLog()
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Target)
+})
+let gForce = 0
+let light2 = 0
+let temp = 0
+let timeStamp = 0
+// Start-Symbol: Zeigt an, dass der Logger bereit ist
+basic.showIcon(IconNames.Target)
+// Spalten in der internen Tabelle definieren
+datalogger.setColumns([
+"Zeit_ms",
+"Temp_C",
+"Licht",
+"G_Kraft"
+])
+// Standard-Zeitstempel deaktivieren, da wir unseren eigenen in Millisekunden nutzen
+datalogger.includeTimestamp(FlashLogTimeStampFormat.None)
 basic.forever(function () {
-    zeit = input.runningTime()
-    temperatur = input.temperature()
-    licht = input.lightLevel()
-    serial.writeLine("" + zeit + "," + temperatur + "," + licht)
-    basic.pause(5000)
+    timeStamp = control.millis()
+    temp = input.temperature()
+    light2 = input.lightLevel()
+    gForce = input.acceleration(Dimension.Strength)
+    // Hier lag der Fehler – jetzt sauber ohne Array-Klammern
+    datalogger.log(
+    datalogger.createCV("Zeit_ms", timeStamp),
+    datalogger.createCV("Temp_C", temp),
+    datalogger.createCV("Licht", light2),
+    datalogger.createCV("G_Kraft", gForce)
+    )
+    led.plot(2, 2)
+    basic.pause(100)
+    led.unplot(2, 2)
+    basic.pause(4900)
 })
